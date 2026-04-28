7ccd8436131f6acb3ca0a64a38edf77dca92ade2
b1tw153 <b.tw153@gmail.com>
2026-04-28 13:16:57 -0700

    Initial query parsing

--- src/qloverleaf/app.py
+++ src/qloverleaf/app.py
@@ -1,14 +1,20 @@
+from urllib.parse import parse_qs
+
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import Route
+from qloverleaf.parser import parse
 
 
-async def interpreter(request: Request) -> Response:
+async def listener(request: Request) -> Response:
     if request.method == "POST":
-        form = await request.form()
-        raw = form.get("data")
-        query = str(raw) if raw is not None else ""
+        body = (await request.body()).decode()
+        if "&" in body:
+            return Response("Unescaped ampersand (&) in query text", status_code=400)
+        params = parse_qs(body, separator="&")
+        print(params)
+        query = params.get("data", [""])[0]
     else:
         query = request.query_params.get("data", "")
 
@@ -16,9 +22,14 @@ async def interpreter(request: Request) -> Response:
         return Response("Missing data parameter", status_code=400)
 
     # TODO: parse and execute query
-    return Response("{}", media_type="application/json")
+    try:
+        tree = parse(query)
+    except Exception as e:
+        return Response(str(e), status_code=400)
+
+    return Response(tree.pretty(), media_type="text/plain")
 
 
 app = Starlette(routes=[
-    Route("/api/interpreter", interpreter, methods=["GET", "POST"]),
+    Route("/api/interpreter", listener, methods=["GET", "POST"]),
 ])

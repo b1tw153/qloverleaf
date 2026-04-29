ec19ec41836dd28262407664fa6c0181082a4c56
b1tw153 <b.tw153@gmail.com>
2026-04-29 09:17:14 -0700

    Rework global settings processing

--- src/qloverleaf/interpreter.py
+++ src/qloverleaf/interpreter.py
@@ -2,59 +2,20 @@ from collections.abc import AsyncGenerator
 
 from lark import Token, Tree
 
-from qloverleaf.query import Query
+from qloverleaf.query import OutputFormat, Query
 
-MEDIA_TYPE_XML = "application/osm3s+xml"
-MEDIA_TYPE_JSON = "application/json"
-MEDIA_TYPE_CSV = "text/csv"
-MEDIA_TYPE_CUSTOM = "text/plain"
-MEDIA_TYPE_POPUP = "application/html"
+MEDIA_TYPES = {
+    OutputFormat.XML: "application/osm3s+xml",
+    OutputFormat.JSON: "application/json",
+    OutputFormat.CSV: "text/csv",
+    OutputFormat.CUSTOM: "text/plain",
+    OutputFormat.POPUP: "application/html",
+}
 
 
 async def initialize(query: Query) -> tuple[str, AsyncGenerator[str, None]]:
-    media_type = get_media_type(query.tree)
-    return media_type, _execute(query)
-
-def get_media_type(tree: Tree[Token]) -> str:
-    media_type = MEDIA_TYPE_XML
-
-    matches = list(tree.find_data("global_output"))
-    if not matches:
-        # TODO: Return the real result when we have formatted output
-        print(media_type)
-        return "text/plain"
-    if len(matches) > 1:
-        # TODO: Make this a QueryError
-        raise Exception("Duplicate global output setting")
-
-    global_output_node = matches[0]
-    assert len(global_output_node.children) == 1, \
-        "Unexpected parsing error: Malformed global output node"
-
-    global_output_setting = global_output_node.children[0]
-    assert isinstance(global_output_setting, Tree), \
-        "Unexpected parsing error: Malformed global output node"
-    global_output_token = global_output_setting.data
-    assert isinstance(global_output_token, Token), \
-        "Unexpected parsing error: Malformed global output token"
-
-    match global_output_token.value:
-        case "global_output_xml":
-            media_type = MEDIA_TYPE_XML
-        case "global_output_json":
-            media_type = MEDIA_TYPE_JSON
-        case "global_output_csv":
-            media_type = MEDIA_TYPE_CSV
-        case "global_output_custom":
-            media_type = MEDIA_TYPE_CUSTOM
-        case "global_output_popup":
-            media_type = MEDIA_TYPE_POPUP
-        case _:
-            assert False, f"Unexpected global output type: {global_output_token.value}"
-
-    # TODO: Return the real result when we have formatted output
-    print(media_type)
-    return "text/plain"
+    _apply_global_settings(query)
+    return MEDIA_TYPES[query.out], _execute(query)
 
 
 def _dump(node: Tree[Token] | Token, indent: int = 0) -> str:
@@ -87,6 +48,118 @@ def _dump(node: Tree[Token] | Token, indent: int = 0) -> str:
     return lines
 
 
+def _apply_global_settings(query: Query) -> None:
+    # apply global timeout
+    matches = list(query.tree.find_data("global_timeout"))
+    if len(matches) > 1:
+        # TODO: Make this a QueryError
+        raise Exception("Duplicate global timeout setting")
+
+    if len(matches) == 1:
+        global_timeout = matches[0]
+        assert len(global_timeout.children) == 1, \
+            f"Unexpected parsing error: Invalid global timeout {global_timeout}"
+        global_timeout_param = global_timeout.children[0]
+        assert isinstance(global_timeout_param, Token), \
+            f"Unexpected parsing error: Invalid global timeout {global_timeout}"
+        query.timeout = int(global_timeout_param.value)
+        print(f"[timeout:{int(global_timeout_param.value)}]")
+
+    # apply global maxsize
+    matches = list(query.tree.find_data("global_maxsize"))
+    if len(matches) > 1:
+        # TODO: Make this a QueryError
+        raise Exception("Duplicate global maxsize setting")
+
+    if len(matches) == 1:
+        global_maxsize = matches[0]
+        assert len(global_maxsize.children) == 1, \
+            f"Unexpected parsing error: Invalid global maxsize {global_maxsize}"
+        global_maxsize_param = global_maxsize.children[0]
+        assert isinstance(global_maxsize_param, Token), \
+            f"Unexpected parsing error: Invalid global maxsize {global_maxsize}"
+        query.maxsize = int(global_maxsize_param.value)
+        print(f"[maxsize:{int(global_maxsize_param.value)}]")
+
+    # apply global date
+    matches = list(query.tree.find_data("global_date"))
+    if len(matches) >= 1:
+        # TODO: Make this a QueryError
+        raise Exception("Unsupported global date setting")
+
+    # apply global diff
+    matches = list(query.tree.find_data("global_diff"))
+    if len(matches) >= 1:
+        # TODO: Make this a QueryError
+        raise Exception("Unsupported global diff setting")
+
+    # apply global adiff
+    matches = list(query.tree.find_data("global_adiff"))
+    if len(matches) >= 1:
+        # TODO: Make this a QueryError
+        raise Exception("Unsupported global adiff setting")
+
+    # apply global output
+    matches = list(query.tree.find_data("global_output"))
+    if len(matches) > 1:
+        # TODO: Make this a QueryError
+        raise Exception("Duplicate global output setting")
+
+    if len(matches) == 1:
+        # walk down to the global_output_* node and get its Token
+        global_output_node = matches[0]
+        assert len(global_output_node.children) == 1, \
+            "Unexpected parsing error: Malformed global output node"
+        global_output_setting = global_output_node.children[0]
+        assert isinstance(global_output_setting, Tree), \
+            "Unexpected parsing error: Malformed global output node"
+        global_output_token = global_output_setting.data
+        assert isinstance(global_output_token, Token), \
+            "Unexpected parsing error: Malformed global output token"
+
+        match global_output_token.value:
+            case "global_output_xml":
+                query.out = OutputFormat.XML
+            case "global_output_json":
+                query.out = OutputFormat.JSON
+            case "global_output_csv":
+                query.out = OutputFormat.CSV
+            case "global_output_custom":
+                query.out = OutputFormat.CUSTOM
+            case "global_output_popup":
+                query.out = OutputFormat.POPUP
+            case _:
+                assert False, \
+                    f"Unexpected global output type: {global_output_token.value}"
+        
+        # collect parameter subtree
+        assert len(global_output_setting.children) <= 1, (
+            "Unexpected parsing error: "
+            f"Invalid global output parameters {global_output_setting}"
+        )
+        if len(global_output_setting.children) == 1:
+            out_params = global_output_setting.children[0]
+            if isinstance(out_params, Tree):
+                query.out_params = out_params
+        print(f"[out:{query.out}({query.out_params})]")
+
+
 async def _execute(query: Query) -> AsyncGenerator[str, None]:
     yield query.tree.pretty()
     yield _dump(query.tree)
+    # walk the top level and collect settings/statements
+    for child in query.tree.children:
+        assert isinstance(child, Tree), \
+            f"Unexpected paring error: Invalid top level element: {child}"
+        data = child.data
+        assert isinstance(data, Token), \
+            f"Unexpected paring error: Invalid top level element: {child}"
+        match data.value:
+            case "global_setting":
+                print(child)
+            case "statement":
+                print(child)
+            case _:
+                assert False, \
+                    f"Unexpected parsing error: Unknown top level element {child}"
+

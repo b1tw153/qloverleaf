f85e66dbcffddae2ef9fd5f66d45cd943c9a0482
b1tw153 <b.tw153@gmail.com>
2026-06-01 06:46:41 -0700

    Add rounding to work around a bug in osm2rdf that stores node WKTs to 6 decimal places instead of 7

--- tests/test_output_formats.py
+++ tests/test_output_formats.py
@@ -28,7 +28,10 @@ def _run_query(query_text: str) -> str:
     return asyncio.run(run())
 
 
-# TODO: Document the limitation that QLever only stores six digits for coordinates
+# QLever only stores six decimal places for POINT coordinates but it stores seven
+# decimal places for WKTs for ways and relations. Reported bug in osm2rdf:
+# https://github.com/ad-freiburg/osm2rdf/issues/135
+# TODO: Remove this rounding when the bug is fixed and QLever is updated
 # TODO: Review type declarations to avoid the Any argument and Any return
 def _round_floats(obj: Any, places: int = 6) -> Any:
     if isinstance(obj, float):
@@ -166,63 +169,63 @@ def test_out_debug_one_stmt() -> None:
 def test_out_json_node_ids() -> None:
     query_text = "[out:json]; node(1); out ids;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 # def test_out_json_node_ids_center() -> None:
 #    query_text = "[out:json]; node(1); out ids center;"
 #    output = _run_query(query_text)
-#    elements = json.loads(output)["elements"]
+#    elements = _round_floats(json.loads(output)["elements"])
 #    assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_node_ids_bb() -> None:
     query_text = "[out:json]; node(1); out ids bb;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_node_tags() -> None:
     query_text = "[out:json]; node(1); out tags;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_node_skel() -> None:
     query_text = "[out:json]; node(1); out skel;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_node_body() -> None:
     query_text = "[out:json]; node(1); out body;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_node_meta() -> None:
     query_text = "[out:json]; node(2); out meta;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_node_geom() -> None:
     query_text = "[out:json]; node(1); out geom;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_node_geom_meta() -> None:
     query_text = "[out:json]; node(2); out geom meta;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
@@ -234,63 +237,63 @@ def test_out_json_node_geom_meta() -> None:
 def test_out_json_way_ids() -> None:
     query_text = "[out:json]; way(100); out ids;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 # def test_out_json_way_ids_center() -> None:
 #    query_text = "[out:json]; way(100); out ids center;"
 #    output = _run_query(query_text)
-#    elements = json.loads(output)["elements"]
+#    elements = _round_floats(json.loads(output)["elements"])
 #    assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_way_ids_bb() -> None:
     query_text = "[out:json]; way(100); out ids bb;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_way_tags() -> None:
     query_text = "[out:json]; way(100); out tags;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_way_skel() -> None:
     query_text = "[out:json]; way(100); out skel;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_way_body() -> None:
     query_text = "[out:json]; way(100); out body;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_way_meta() -> None:
     query_text = "[out:json]; way(100); out meta;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_way_geom() -> None:
     query_text = "[out:json]; way(100); out geom;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_way_geom_meta() -> None:
     query_text = "[out:json]; way(100); out geom meta;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
@@ -302,63 +305,63 @@ def test_out_json_way_geom_meta() -> None:
 def test_out_json_rel_ids() -> None:
     query_text = "[out:json]; rel(18375544); out ids;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 # def test_out_json_rel_ids_center() -> None:
 #    query_text = "[out:json]; rel(18375544); out ids center;"
 #    output = _run_query(query_text)
-#    elements = json.loads(output)["elements"]
+#    elements = _round_floats(json.loads(output)["elements"])
 #    assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_rel_ids_bb() -> None:
     query_text = "[out:json]; rel(11837554400); out ids bb;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_rel_tags() -> None:
     query_text = "[out:json]; rel(18375544); out tags;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_rel_skel() -> None:
     query_text = "[out:json]; rel(18375544); out skel;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_rel_body() -> None:
     query_text = "[out:json]; rel(18375544); out body;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_rel_meta() -> None:
     query_text = "[out:json]; rel(18375544); out meta;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_rel_geom() -> None:
     query_text = "[out:json]; rel(18375544); out geom;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
 def test_out_json_rel_geom_meta() -> None:
     query_text = "[out:json]; rel(18375544); out geom meta;"
     output = _run_query(query_text)
-    elements = json.loads(output)["elements"]
+    elements = _round_floats(json.loads(output)["elements"])
     assert elements == _overpass_elements(query_text)
 
 
